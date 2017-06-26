ba2d93c9141458149cc11afd2914aeb29a3156fe
liupeng <liupeng_internet@163.com>
2017-06-26 10:38:32 +0800

    Add： 讲解面向对象实例

--- WhatOOP/Program.cs
+++ WhatOOP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Baidu.Aip.Face;
 using Baidu.Aip.Nlp;
 using MySoft.Logger;
 using Newtonsoft.Json;
@@ -16,8 +17,38 @@ namespace WhatOOP
         private static void Main(string[] args)
         {
             //SentimentClassify();
-            var result = GenericFunction.Get<string>(2, "2");
-            Console.WriteLine(result.GetType());
+            //var result = GenericFunction.Get<string>(2, "2");
+            //Console.WriteLine(result.GetType());
+            //var liupengSon = new Son();
+            //liupengSon.UserName = "刘鹏";
+            //liupengSon.BuildHouse();
+            //liupengSon.ShowSkill();
+
+            //int num = 1;
+            //long numLong = 2;
+            //string str = "";
+            //float numFloat = 1.5f;
+            //double numDouble = 1.3;
+            //decimal numDecimal = 2.5M;
+            //object obj  = "";
+            //bool numBool = true;
+
+
+            //var userName = "liupeng";
+            //运算符+、-、*、/，
+
+            var numOne = 2;
+            var numThree = numOne --;
+            numThree += numOne;
+            numThree = numThree + numOne;
+            numThree -= numOne;
+            numThree = numThree - numOne;
+            //Console.WriteLine(numTwo);
+            Console.WriteLine(numOne);
+            Console.WriteLine(numThree);
+
+
+            
 
             Console.ReadKey();
         }
@@ -27,12 +58,36 @@ namespace WhatOOP
             var nlp = new Nlp(AppKey, AppSecret);
             const string sqlStr = "SELECT  id,accountid,t_mk,vm_id FROM  dbo.Sys_TaskDaily";
             var taskDailyList = DapperHelper.Query<TaskDailyEntity>(sqlStr, null);
-            foreach (var result in from item in taskDailyList where !string.IsNullOrWhiteSpace(item.T_mk) select nlp.SentimentClassify(item.T_mk))
+            foreach (
+                var result in
+                    from item in taskDailyList
+                    where !string.IsNullOrWhiteSpace(item.T_mk)
+                    select nlp.SentimentClassify(item.T_mk))
             {
                 SimpleLog.Instance.WriteLogForFile("用户反馈情感分析", JsonConvert.SerializeObject(result));
             }
-            
+
             Console.WriteLine("执行完毕！");
+        }
+    }
+
+
+    public class Father
+    {
+        public string UserName;
+
+        public void BuildHouse()
+        {
+            Console.WriteLine("我能建房子！，我的名字是：" + UserName);
+        }
+    }
+
+
+    public class Son : Father
+    {
+        public void ShowSkill()
+        {
+            Console.WriteLine("我可以打dota");
         }
     }
 }
